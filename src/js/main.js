16fb728b7ec7d6ba8476988f94de51240b5dbaf2
Olya Us <olyaus0809@gmail.com>
2022-10-24 17:13:24 +0300

    Show images

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,8 @@ import modals from './modules/modals';
 import tabs from './modules/tabs';
 import forms from './modules/forms';
 import changeModalState from './modules/changeModalState';
-import timer from './modules/timer'
+import timer from './modules/timer';
+import images from './modules/images';
 
 window.addEventListener('DOMContentLoaded', () => {
     "use strict";
@@ -17,5 +18,6 @@ window.addEventListener('DOMContentLoaded', () => {
     tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
     tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
     forms(modalState);
-    timer('.container1', deadline)
+    timer('.container1', deadline);
+    images();
 });
